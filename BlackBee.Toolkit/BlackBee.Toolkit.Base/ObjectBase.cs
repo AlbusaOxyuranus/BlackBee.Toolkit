75fc69fb5eb7b21117c43f883bb6629b16df7628
Denis Prokhorchik <dionis_999@hotmail.com>
2018-03-21 13:45:37 +0300

    added base class  ObjectBase

---
+++ BlackBee.Toolkit/BlackBee.Toolkit.Base/ObjectBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using BlackBee.Toolkit.Base.Annotations;
+
+namespace BlackBee.Toolkit.Base
+{
+    public class ObjectBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
