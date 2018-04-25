70d9e5dd679cc5e5b58361e8b81c99b09723266c
Denis Prokhorchik <dionis_999@hotmail.com>
2018-04-25 22:16:56 +0300

    fixed namespace

--- scr/BlackBee.Toolkit/UWP/Toolkit.Tests/BlackBee.Toolkit.Base.UWP.Tests/Properties/AssemblyInfo.cs
+++ scr/BlackBee.Toolkit/UWP/Toolkit.Tests/BlackBee.Toolkit.Base.UWP.Tests/Properties/AssemblyInfo.cs
@@ -10,11 +10,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("85aff1aa-04e9-47b5-9233-d51d19b29164")]
+[assembly: AssemblyMetadata("TargetPlatform","UAP")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
---
+++ scr/BlackBee.Toolkit/UWP/Toolkit.Tests/BlackBee.Toolkit.Base.UWP.Tests/UnitTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlackBee.Toolkit.Base.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
